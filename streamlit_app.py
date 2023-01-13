d02ed2ea8fb7b3d15cabed9a9f2e44b022b332d8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-01-13 23:54:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Slides App')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vTlu3-j10StHZe5LxOTp3aRdnBZ1-sqlGXm-3ulVZzTq6qSqFO4NNsil-_EWgoDv9OcofVpPhOClLpk/embed?start=false&loop=false&delayms=3000", height=520)
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vTlu3-j10StHZe5LxOTp3aRdnBZ1-sqlGXm-3ulVZzTq6qSqFO4NNsil-_EWgoDv9OcofVpPhOClLpk/embed?start=false&loop=false&delayms=3000", height=550)
