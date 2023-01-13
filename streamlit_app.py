85b7f53be6226eec89f931e194775ba45f62dfe0
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-01-13 23:40:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.title('🎈 App Name')
+st.title('🎈 Slides App')
 
-st.write('Hello world!')
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vTlu3-j10StHZe5LxOTp3aRdnBZ1-sqlGXm-3ulVZzTq6qSqFO4NNsil-_EWgoDv9OcofVpPhOClLpk/embed?start=false&loop=false&delayms=3000")
