dd895688bee076497574535d76b4665640d5d601
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-03-14 19:10:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation App')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vTlu3-j10StHZe5LxOTp3aRdnBZ1-sqlGXm-3ulVZzTq6qSqFO4NNsil-_EWgoDv9OcofVpPhOClLpk/embed?start=false&loop=false&delayms=3000", height=565)
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQCqeDL5PnYToA6hPJ5YlcT1MiLyqhSqk4S0xqnMmDZ44KYhJ_nYGOZl1gRcz1bhncgN1OilHgi7AjJ/embed?start=false&loop=false&delayms=5000", height=565)
